99761119422c1dce43fce769758b3503bcd1e846
Wen Liang - Desktop <wenlianggg@gmail.com>
2016-05-28 19:21:28 +0800

    Small issues fix

--- MasterBox/Site.Master.designer.cs
+++ MasterBox/Site.Master.designer.cs
@@ -65,5 +65,14 @@ namespace MasterBox {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label LoggedInUser;
+        
+        /// <summary>
+        /// IPAddr control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label IPAddr;
     }
 }
