b24ae0a78d3d4ce7b79e1f7859d68971ec5a7561
Wen Liang - Desktop <wenlianggg@gmail.com>
2016-05-28 09:26:27 +0800

    Enforce site authentication

--- MasterBox/Site.Master.designer.cs
+++ MasterBox/Site.Master.designer.cs
@@ -22,6 +22,15 @@ namespace MasterBox {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
         
         /// <summary>
+        /// WelcomeBack control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label WelcomeBack;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
