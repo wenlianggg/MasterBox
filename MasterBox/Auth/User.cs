a449849766cf6f31afaa1303962bc50538fbce3b
Wen Liang - Desktop <wenlianggg@gmail.com>
2016-06-12 16:38:17 +0800

    Simple entity class

---
+++ MasterBox/Auth/User.cs
@@ -0,0 +1,23 @@
+﻿using MasterBox.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MasterBox {
+	public class User {
+		String username;
+		Boolean isTOTPEnabled;
+		String TOTPToken;
+		Boolean isLoggedIn;
+		Boolean is2FAEnabled;
+		Boolean is2FAAuthed;
+		private static AuthDBControlDataContext adb = new AuthDBControlDataContext();
+
+		public User(String username, String ptPass) {
+			adb.W
+
+		}
+
+	}
+}
