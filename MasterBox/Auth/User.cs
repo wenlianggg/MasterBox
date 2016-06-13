6eeffac426e52e3db3dd2c757e5de8cad8bf2dbd
User <User@MSI>
2016-06-13 15:09:55 +0800

    Major Change Pls revert if got taiji :)

--- MasterBox/Auth/User.cs
+++ MasterBox/Auth/User.cs
@@ -3,14 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Security;
 
 namespace MasterBox {
-	public class User : MembershipUser {
+	public class User {
+		String username;
+		Boolean isTOTPEnabled;
+		String TOTPToken;
+		Boolean isLoggedIn;
+		Boolean is2FAEnabled;
+		Boolean is2FAAuthed;
 		private static AuthDBControlDataContext adb = new AuthDBControlDataContext();
 
 		public User(String username, String ptPass) {
-			
+		
 
 		}
 
