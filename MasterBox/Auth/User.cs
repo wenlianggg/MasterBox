f37fd7db8a00fca8b7a2794bb795e79a4c9f00fb
Wen Liang - Desktop <wenlianggg@gmail.com>
2016-06-13 20:00:03 +0800

    MBProvider User Validation (TODO: Salt)

--- MasterBox/Auth/User.cs
+++ MasterBox/Auth/User.cs
@@ -7,12 +7,6 @@ using System.Web.Security;
 
 namespace MasterBox.Auth {
 	public class User : MembershipUser {
-		String username;
-		Boolean isTOTPEnabled;
-		String TOTPToken;
-		Boolean isLoggedIn;
-		Boolean is2FAEnabled;
-		Boolean is2FAAuthed;
 		private static AuthDBControlDataContext adb = new AuthDBControlDataContext();
 
 		public User(String username, String ptPass) {
