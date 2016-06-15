8645b7c8c8666de336a801fc974d665b4e2df93b
froststein <155125Y@mymail.nyp.edu.sg>
2016-06-15 10:53:12 +0800

    Created a file object

---
+++ MasterBox/mbox/File.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MasterBox.mbox
+{
+    class File
+    {
+        public string fileName { get; set; }
+        public string fileType { get; set; }
+        public byte fileSize { get; set; }
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+           
+        }
+        
+
+
+    }
+}
