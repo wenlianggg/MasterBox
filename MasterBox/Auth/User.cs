757cb46bc5f1dd2d910d218a3c77774ad94346fd
Wen Liang - Desktop <wenlianggg@gmail.com>
2016-06-30 01:38:07 +0800

    Serialization code

--- MasterBox/Auth/User.cs
+++ MasterBox/Auth/User.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Web;
 using System.Web.Security;
 
@@ -302,7 +304,27 @@ namespace MasterBox.Auth {
 				RefreshAllFields();
 			}
 		}
+		private byte[] ObjectToByteArray(Object obj) {
+			if (obj == null)
+				return null;
 
+			BinaryFormatter bf = new BinaryFormatter();
+			MemoryStream ms = new MemoryStream();
+			bf.Serialize(ms, obj);
+
+			return ms.ToArray();
+		}
+
+		// Convert a byte array to an Object
+		private Object ByteArrayToObject(byte[] arrBytes) {
+			MemoryStream memStream = new MemoryStream();
+			BinaryFormatter binForm = new BinaryFormatter();
+			memStream.Write(arrBytes, 0, arrBytes.Length);
+			memStream.Seek(0, SeekOrigin.Begin);
+			Object obj = (Object)binForm.Deserialize(memStream);
+
+			return obj;
+		}
 
 	}
 
