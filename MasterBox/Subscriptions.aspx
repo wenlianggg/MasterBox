0fad9e4469d55f1c6da4f5ac1fad536c1767945d
T3hcamp3r <153010N@mymail.Nyp.edu.sg>
2016-07-04 16:57:31 +0800

    Created the subscriptions page, incomplete

--- MasterBox/Subscriptions.aspx
+++ MasterBox/Subscriptions.aspx
@@ -1,5 +1,30 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Subscriptions.aspx.cs" Inherits="MasterBox.Subscriptions" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="jumbotron">
+        <h1>Your Subscriptions</h1>
+        <p>Manage your payments subscriptions and storage space.</p>
+    </div>
+    <h1 id="TotalSpace" runat="server"></h1>
+    <div class="panel panel-default">
+        <div class="panel-heading"><strong>Storage</strong></div>
+        <div class="panel-body">
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped active" role="progressbar" id="Bar"
+                    aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 40%" runat="server">
+                </div>
+            </div>
+            <h4 runat="server" id="Available"></h4>
+            <asp:Button OnClick="GoToFiles" runat="server" CssClass="btn btn-info" Text="Go To Files" />
+        </div>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-heading"><strong>Subscription Plans</strong></div>
+        <div class="panel-body">
+            <p runat="server" id="FreeSpace"></p>
+            <asp:Button runat="server" CssClass="btn btn-info" Text="Purchase Space" OnClick="GoToPrices" />
+        </div>
+    </div>
 </asp:Content>
--- MasterBox/Subscriptions.aspx.cs
+++ MasterBox/Subscriptions.aspx.cs
@@ -11,7 +11,53 @@ namespace MasterBox
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Progress Bar Values //
+            double freespace = 5;
+            double actualspace = 1000 + freespace;
+            double spaceused = 650;
+            double percentused = (spaceused / actualspace) * 100;
+            double availablespace = actualspace - spaceused;
+            var roundedused = Math.Round(percentused, 1);
 
+            if(roundedused <= 40)
+            {
+                TotalSpace.InnerText = actualspace + " MB Total";
+                Bar.Attributes["style"] = "width:" + roundedused + "%";
+                Bar.Attributes["class"] = "progress-bar progress-bar-success progress-bar-striped active";
+                Bar.InnerText = Convert.ToString(roundedused) + "%";
+                Available.InnerText = availablespace + " MB available";
+            }
+            else if (roundedused <= 70)
+            {
+                TotalSpace.InnerText = actualspace + " MB Total";
+                Bar.Attributes["style"] = "width:" + roundedused + "%";
+                Bar.Attributes["class"] = "progress-bar progress-bar-warning progress-bar-striped active";
+                Bar.InnerText = Convert.ToString(roundedused) + "%";
+                Available.InnerText = availablespace + " MB available";
+            }
+            else
+            {
+                TotalSpace.InnerText = actualspace + " MB Total";
+                Bar.Attributes["style"] = "width:" + roundedused + "%";
+                Bar.Attributes["class"] = "progress-bar progress-bar-danger progress-bar-striped active";
+                Bar.InnerText = Convert.ToString(roundedused) + "%";
+                Available.InnerText = availablespace + " MB available";
+            }
+            
+   
+
+            // Subscription Plans Values //
+            FreeSpace.InnerText = "Free Space Given: " + freespace + "MB";
+        }
+
+        protected void GoToFiles(object sender, EventArgs e)
+        {
+            Response.Redirect("~/mbox/FileTransferInterface.aspx");
+        }
+
+        protected void GoToPrices(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Pricing.aspx");
         }
     }
 }
