2e01825f2dfea505af6de52bb1734a1b1758c193
T3hcamp3r <153010N@mymail.Nyp.edu.sg>
2016-07-08 00:02:33 +0800

    Fixed dat payment shit boys. Can show value of item bought in success page

---
+++ MasterBox/SpecChar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace MasterBox
+{
+    public class SpecChar
+    {
+            public static string CleanInput(string strIn)
+                 {
+                // Replace invalid characters with empty strings.
+                try
+                {
+                    return Regex.Replace(strIn, @"[^A-Za-z0-9]+", " ",
+                                         RegexOptions.None, TimeSpan.FromSeconds(1.5));
+                }
+                // If we timeout when replacing invalid characters, 
+                // we should return Empty.
+                catch (RegexMatchTimeoutException)
+                {
+                    return String.Empty;
+                }
+            }
+        }
+}
