e85e55d8ac3f4fa690d3447505298e653d93dbe9
Wilfred <wilfredkang@live.com>
2016-07-21 10:55:45 +0800

    ,,|,,

---
+++ MasterBox/FileSharing.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Title="File Sharing" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="TestPage.aspx.cs" Inherits="MasterBox.filestore.WebForm1" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div>
+        <asp:Label runat="server" id="HelloWorldLabel"></asp:Label>
+        <br />
+        <asp:Label runat="server" id="VerifyLabel"></asp:Label>
+        <br />
+        <asp:Label runat="server" id="Check"></asp:Label>
+        <br />
+        <asp:Button runat="server" ID="DLButton" Text="Download"></asp:Button>
+        <asp:Button runat="server" ID="UPLButton" Text="Upload"></asp:Button>
+        <asp:Button runat="server" ID="DELButton" Text="Delete"></asp:Button>
+        <br />
+        <asp:Label runat="server" ID="CountDown"></asp:Label>
+    </div>
+
+</asp:Content>
---
+++ MasterBox/filestore/LinkShare.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MasterBox.fileshare
+{
+    public class LinkShare
+    {
+        public string link { get; set; }
+        public DateTime dt { get; set; }
+        public FileAccess fa { get; set; }
+        
+        public LinkShare()
+        {
+
+        }
+
+        public LinkShare(string link, DateTime dt, FileAccess fa)
+        {
+            this.link = link;
+            this.dt = dt;
+            this.fa = fa;
+        }
+
+        public LinkShare(string link, FileAccess fa)
+        {
+            this.link = link;
+            this.fa = fa;
+        }
+        public LinkShare(string link, DateTime dt, long userid, long folderid, bool download, bool upload, bool delete)
+        {
+            this.link = link;
+            this.dt = dt;
+            fa = new FileAccess(userid, folderid, download, upload, delete);
+        }
+
+        private static SqlConnection SQLGetMBoxConnection()
+        {
+            SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["MBoxCString"].ConnectionString);
+            sqlConnection.Open();
+            return sqlConnection;
+        }
+
+        public LinkShare RetrieveLinkShare(string link)
+        {
+            LinkShare ls = new LinkShare();
+
+            SqlCommand cmd = new SqlCommand(
+                "SELECT * FROM linkshare WHERE link = @link", SQLGetMBoxConnection());
+            cmd.Parameters.Add(new SqlParameter("@link", SqlDbType.VarChar, 16));
+            cmd.Prepare();
+            cmd.Parameters["@link"].Value = link;
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+                DateTime dt = (DateTime)reader["dt"]; ;
+                long userid = (long)reader["userid"];
+                long folderid = (long)reader["folderid"];
+                bool download = (bool)reader["download"];
+                bool upload = (bool)reader["upload"];
+                bool delete = (bool)reader["delete"];
+                ls = new LinkShare(link, dt, userid, folderid, download, upload, delete);
+            }
+
+            return ls;
+        }
+
+        public void UploadLinkShare()
+        {
+            SqlCommand cmd = new SqlCommand(
+                "INSERT INTO linkshare(link, dt, userid, folderid, download, upload, delete) VALUES(@link, NOW(), @userid, @folderid, @download, @upload, @delete", SQLGetMBoxConnection());
+            cmd.Parameters.AddWithValue("@link", link);
+            cmd.Parameters.AddWithValue("@userid", fa.userid);
+            cmd.Parameters.AddWithValue("@folderid", fa.folderid);
+            cmd.Parameters.AddWithValue("@download", fa.download);
+            cmd.Parameters.AddWithValue("@upload", fa.upload);
+            cmd.Parameters.AddWithValue("@delete", fa.delete);
+
+            //SqlDataReader reader = cmd.ExecuteReader();
+
+            System.Diagnostics.Debug.WriteLine("LinkShare created.");
+        }
+
+        public void CreateFileAccess()
+        {
+            SqlCommand cmd = new SqlCommand(
+                "INSERT INTO fileaccess(userid, folderid, download, upload, delete) VALUES(@link, NOW(), @userid, @folderid, @download, @upload, @delete", SQLGetMBoxConnection());
+            cmd.Parameters.AddWithValue("@userid", fa.userid);
+            cmd.Parameters.AddWithValue("@folderid", fa.folderid);
+            cmd.Parameters.AddWithValue("@download", fa.download);
+            cmd.Parameters.AddWithValue("@upload", fa.upload);
+            cmd.Parameters.AddWithValue("@delete", fa.delete);
+
+            //SqlDataReader reader = cmd.ExecuteReader();
+
+            System.Diagnostics.Debug.WriteLine("FileAccess created.");
+        }
+
+        public string Verify(long userid)
+        {
+            string str = "";
+            if (!CheckUser(userid))
+            {
+               str += "You are not the intended recipient of this invitation link. ";
+            }
+            
+            if (!CheckDate())
+            {
+                System.Diagnostics.Debug.WriteLine("Run CheckDate;");
+                str += "Invitation link has timed out.";
+            }
+            return str;
+        }
+        public bool CheckUser(long userid)
+        {
+            if (userid == fa.userid)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool CheckDate()
+        {
+            TimeSpan difference = DateTime.Now - dt;
+
+            System.Diagnostics.Debug.WriteLine("TimeSpan difference is: " + difference);
+
+            if (difference.TotalDays < 1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
