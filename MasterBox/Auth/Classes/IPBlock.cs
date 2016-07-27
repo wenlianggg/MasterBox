4bc96a77c130291a7f56106eec5014dba3c5a207
Wen Liang - Desktop <wenlianggg@gmail.com>
2016-07-28 01:22:30 +0800

    BACK AT WOORK

--- MasterBox/Auth/Classes/IPBlock.cs
+++ MasterBox/Auth/Classes/IPBlock.cs
@@ -8,7 +8,7 @@ namespace MasterBox.Auth {
 	public class IPBlock {
 
 		private IPBlock _instance;
-		private List<IPBlockEntry> _blocklist;
+		private List<IPBlockEntry> bList;
 
 		public IPBlock Instance {
 			get {
@@ -18,10 +18,11 @@ namespace MasterBox.Auth {
 			}
 		}
 
+
 		private IPBlock() {
 			using (DataAccess da = new DataAccess()) {
 				SqlDataReader sqldr = da.SqlGetBlockList();
-				_blocklist = new List<IPBlockEntry>();
+				bList = new List<IPBlockEntry>();
 				while (sqldr.Read()) {
 					if ((DateTime) sqldr["expiry"] > DateTime.Now) {
 						IPBlockEntry ipbe = new IPBlockEntry(
@@ -29,7 +30,7 @@ namespace MasterBox.Auth {
 							ipaddress: sqldr["address"].ToString(),
 							expiry: (DateTime)sqldr["expiry"],
 							reason: sqldr["reason"].ToString());
-						_blocklist.Add(ipbe);
+						bList.Add(ipbe);
 					}
 				}
 			}
@@ -37,7 +38,7 @@ namespace MasterBox.Auth {
 
 		// Username and IP combination block check
 		internal bool Check(int userid, string ip) {
-			foreach (IPBlockEntry ipbe in _blocklist) {
+			foreach (IPBlockEntry ipbe in bList) {
 				if (ipbe.UserID.Equals(userid) && ipbe.IPAddress.Equals(ip))
 					return true;
 			}
@@ -46,7 +47,7 @@ namespace MasterBox.Auth {
 
 		// Username only block check
 		internal bool Check(int userid) {
-			foreach (IPBlockEntry ipbe in _blocklist) {
+			foreach (IPBlockEntry ipbe in bList) {
 				if (ipbe.UserID.Equals(userid) && ipbe.IPAddress == null)
 					return true;
 			}
@@ -55,16 +56,45 @@ namespace MasterBox.Auth {
 
 		// IP only block check
 		internal bool Check(string ip) {
-			foreach (IPBlockEntry ipbe in _blocklist) {
+			foreach (IPBlockEntry ipbe in bList) {
 				if (ipbe.IPAddress.Equals(ip) && ipbe.UserID == 0)
 					return true;
 			}
 			return false;
 		}
 
-		// TODO: Adding to blocklist
+		// Adding to blocklist (IP Address Only)
+		internal void Create(string ip, TimeSpan? ts = null, string reason = "Unspecified reason") {
+			DateTime expiry;
+			if (ts != null)
+				expiry = DateTime.Now.Add(ts.Value);
+			else
+				expiry = DateTime.Now.AddYears(99);
+			IPBlockEntry newipbe = new IPBlockEntry(0, ip, expiry, reason);
+			
+		}
 
-		// TODO: Removing from blocklist
+		// Adding to blocklist (User ID Only)
+		internal void Create(int userid, TimeSpan? ts = null, string reason = "Unspecified reason") {
+			DateTime expiry;
+			if (ts != null)
+				expiry = DateTime.Now.Add(ts.Value);
+			else
+				expiry = DateTime.Now.AddYears(99);
+			IPBlockEntry newipbe = new IPBlockEntry(0, expiry: expiry, reason: reason);
+		}
+
+		// Adding to blocklist (User ID and IP address combination)
+		internal void Create(int userid, string ipaddress, TimeSpan? ts = null, string reason = "Unspecified reason") {
+			DateTime expiry;
+			if (ts != null)
+				expiry = DateTime.Now.Add(ts.Value);
+			else
+				expiry = DateTime.Now.AddYears(99);
+			IPBlockEntry newipbe = new IPBlockEntry(0, ipaddress, expiry, reason);
+		}
+		
+		// TODO: Blocklist removal
 
 	}
 }
