66bde269bfe8fdf56072b0d9011696c9d6d9ac46
Dominic <150501D@mymail.nyp.edu.sg>
2016-08-02 11:30:35 +0800

    sender email in teh subject

--- MasterBox/Contact.aspx.cs
+++ MasterBox/Contact.aspx.cs
@@ -20,7 +20,7 @@ namespace MasterBox
             MailMessage Msg = new MailMessage();
             Msg.From = new MailAddress("masterboxnoreply@gmail.com");
             Msg.To.Add(txtToMail.Text);
-            Msg.Subject = txtSubject.Text + "(From: " + txtFrom.Text + ")";
+            Msg.Subject = txtSubject.Text + "  ( From: " + txtFrom.Text + " ) ";
             //Msg.Subject = txtSubject.Text;
             Msg.Body = txtMessage.Text;
             Msg.IsBodyHtml = true;
