71218714ea4b2a4528569f8b5e8d559fafc22200
Dominic <150501D@mymail.nyp.edu.sg>
2016-08-07 15:49:13 +0800

    asasd

--- MasterBox/admin/Classes/Mail.cs
+++ MasterBox/admin/Classes/Mail.cs
@@ -2,35 +2,68 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+//using MailMessage=System.Net.Mail.MailMessage;
 
 namespace MasterBox.Admin {
 	public class Mail {
 
 		// Getter and setters (fields)
 
-		internal string Sender { get; set; }
-		internal string Recipient { get; set; }
-		internal string Title { get; set; } 
-		internal string Body { get; set; }
+        string MailSmtpHost { get; set; }
+        int MailSmtpPort { get; set; }
+        string MailSmtpUsername { get; set; }
+        string MailSmtpPassword { get; set; }
+        string MailFrom { get; set; }
+
+        //internal string From { get; set; }
+		//internal string To { get; set; }
+		//internal string Subject { get; set; } 
+		//internal string Body { get; set; }
 
 		// Constructors, creates mail object
-
-		internal Mail(string recipient, string title, string body) {
-			Sender = "jefferyballstester...";
-			Recipient = recipient;
-			Title = title;
+        /*
+		internal Mail(string to, string subject, string body) {
+			From = "masterboxnoreply@gmail.com";
+			To = to;
+			Subject = subject;
 			Body = body;
 			// Start email server connection or whatnot
 		}
+        */
 
-		// Methods for mail class
-		internal bool Send() {
+        public bool SendEmail(string to, string subject, string body)
+        {
+            MailMessage mail = new MailMessage(MailFrom, to, subject, body);
+            var alternameView = AlternateView.CreateAlternateViewFromString(body, new ContentType("text/html"));
+            mail.AlternateViews.Add(alternameView);
+
+            var smtpClient = new SmtpClient(MailSmtpHost, MailSmtpPort);
+            smtpClient.Credentials = new NetworkCredential(MailSmtpUsername, MailSmtpPassword);
+            try
+            {
+                smtpClient.Send(mail);
+            }
+            catch (Exception e)
+            {
+                //Log errors here
+                return false;
+            }
+
+            return true;
+        }
+
+        /*
+        // Methods for mail class
+        internal bool Send() {
 			// Sends the mail
 
 			// Returns true if sent
 			return false;
 		}
-
+        */
 		// ... any other methods
 	}
 }
